5dbb5d314f55a65eee46019e2fbb0f225b736e1b
gunter.oliveira@ab-inbev.com <gunter.oliveira@ab-inbev.com>
2021-08-23 16:04:56 -0300

    first commit

---
+++ src/test/java/com/gunter/GenericCRUD/GenericCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gunter.GenericCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenericCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
